12a757b3a792da6f849d846df1b0bb5b61bf4368
9ot9am <9ot9am@gmail.com>
2025-03-25 23:23:07 +0900

    chore(slime):name

--- Assets/4. Scripts/UI/QuestData.cs
+++ Assets/4. Scripts/UI/QuestData.cs
@@ -13,7 +13,7 @@ public class QuestData : MonoBehaviour
 
     public Dictionary<int, string> questScripts = new Dictionary<int, string>()
     {
-        { 1001, "Thank you for saving me. I saw the giant monster kidnapped someone.\n" +
+        { 1001, "Please Let ME Go....\n" +
                 "I guess that someone is who your looking for.\n" +
                 "Why don't you just go to deal with him?\n" }
     };
